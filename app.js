055efedcda3dccc11aab925edaec22f631162d12
Rachit Jain <220160212042.rachit@gdgu.org>
2024-10-05 03:05:06 +0530

    Corrected Model

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cookie = require('cookie-parser');
 const path = require('path');
 const userModel = require('./models/user');
 
 app.set("view engine", "ejs");
 app.use(express.json());
-app.use(express.urlencoded({ extended : true}));
+app.use(express.urlencoded({ extended : true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
@@ -15,24 +19,36 @@ app.get('/', (req, res) => {
 app.get('/signin', (req, res) => {
     res.render("signin");
 })
+
 app.get('/signup', (req, res) => {
     res.render("signup");
 })
 
-
 app.post('/create', async (req, res) => {
-    let {username, email, password} = req.body;
+    let { username, email, password, isFreelancer } = req.body;
 
-    let createdUser = await userModel.create({
-        username, 
-        email, 
-        password
-    });
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, async (err, hash) => {
+            let createdUser = await userModel.create({
+                username,
+                email,
+                password: hash,
+                isFreelancer
+            });
+        })
+    })
+
+    let token = jwt.sign({ email }, "My_Secret_Token");
+    res.cookie("token", token);
 
     res.send(createdUser);
-    console.log(createdUser)
+})
+
+app.post('/logout', async (req, res) => {
+    res.cookie("token", "");
+    res.redirect("/");
 })
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+    console.log('Server is running on port 3000');
 })
--- models/user.js
+++ models/user.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/freelancer");
+mongoose.connect("mongodb://127.0.0.1:27017/FreelanceChowk");
 
 const userSchema = mongoose.Schema({
     username : String,
     email : String,
-    password : String
+    password : String,
+    isFreelancer : Boolean
 })
 
 module.exports = mongoose.model('user', userSchema);
