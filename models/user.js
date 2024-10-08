934b09901c1f0b3ab194792a823501ad093be7c1
Rachit Jain <rachitjain75.rj75@gmail.com>
2024-10-09 01:50:10 +0530

    I don't remenber what I did but progress is done

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const freelancer = require('./freelancer');
 
 mongoose.connect("mongodb://127.0.0.1:27017/FreelanceChowk");
 
@@ -6,7 +7,15 @@ const userSchema = mongoose.Schema({
     username : String,
     email : String,
     password : String,
-    isFreelancer : Boolean
+    isFreelancer : Boolean,
+    FL_details : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "freelancer"
+    },
+    C_details : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "client"
+    }
 })
 
 module.exports = mongoose.model('user', userSchema);
